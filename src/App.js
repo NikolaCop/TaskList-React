2fcdcbe29257a185f56764d0ce1c6e9485c6ccac
Nikola Cop <nikolacop05@gmail.com>
2021-08-02 12:23:26 -0600

    Setting State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
-
+import React, { useState } from 'react';
+import TodoList from'./TodoList';
 function App() {
+  const [todos, setTodos] = useState([])
   return (
+    <>
     <TodoList />
-  )
+    <input type="text" />
+    <button>Add Todo</button>
+    <button>Clear Complete</button>
+    <div>0 left to do</div>
+    </> 
+       )
 }
 
 
